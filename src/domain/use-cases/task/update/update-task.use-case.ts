d3802fb420253692779b10b28558cfd1127160cb
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2023-07-16 16:11:51 -0300

    feat: ensure that UpdateTaskUseCase throws a NotFoundException if no task was found

--- src/domain/use-cases/task/update/update-task.use-case.ts
+++ src/domain/use-cases/task/update/update-task.use-case.ts
@@ -1,10 +1,18 @@
-import { ForbiddenException } from '@nestjs/common'
+import {
+  ForbiddenException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
 
 import { UpdateTaskCommand, UpdateTaskOutput } from './update-task.dto'
 import { TaskRepository } from '../../../interfaces'
 
+@Injectable()
 export class UpdateTaskUseCase {
-  constructor(private readonly taskRepository: TaskRepository) {}
+  constructor(
+    @Inject('TaskRepository') private readonly taskRepository: TaskRepository,
+  ) {}
 
   async execute({
     id,
@@ -15,6 +23,7 @@ export class UpdateTaskUseCase {
     status,
   }: UpdateTaskCommand): Promise<UpdateTaskOutput> {
     const task = await this.taskRepository.findById(id)
+    if (!task) throw new NotFoundException()
     if (task.userId !== userId) throw new ForbiddenException()
     task.update(title, description, status, dueDate)
     await this.taskRepository.update(task)
