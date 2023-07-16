0d74542a5672795d511670069631af61b87f7b34
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2023-07-16 14:12:17 -0300

    test: ensure that ListTasksUseCase is implemented correctly

---
+++ src/domain/use-cases/task/list/list-tasks.spec.ts
@@ -0,0 +1,51 @@
+import { MockProxy, mock } from 'jest-mock-extended'
+import { randomUUID } from 'crypto'
+
+import { ListTasksUseCase } from './list-tasks.use-case'
+import { TaskRepository } from '../../../interfaces'
+import { Task } from '../../../entities'
+import { TaskStatus } from '../../../enums'
+
+describe('ListTasksUseCase', () => {
+  let sut: ListTasksUseCase
+  let mockedTaskRepository: MockProxy<TaskRepository>
+  let fakeTask: Task
+
+  beforeAll(() => {
+    mockedTaskRepository = mock()
+    fakeTask = Task.newTask(
+      'any_title',
+      'any_description',
+      new Date(),
+      TaskStatus.CREATED,
+      randomUUID(),
+    )
+    mockedTaskRepository.list.mockResolvedValue([fakeTask])
+  })
+
+  beforeEach(() => {
+    sut = new ListTasksUseCase(mockedTaskRepository)
+  })
+
+  it('should call TaskRepository.list correctly', async () => {
+    const command = {
+      userId: randomUUID(),
+    }
+
+    await sut.execute(command)
+
+    expect(mockedTaskRepository.list).toHaveBeenCalledTimes(1)
+    expect(mockedTaskRepository.list).toHaveBeenCalledWith(command.userId)
+  })
+
+  it('should return a Task list on success', async () => {
+    const expectedTaskList = [fakeTask]
+    const command = {
+      userId: randomUUID(),
+    }
+
+    const actualTaskList = await sut.execute(command)
+
+    expect(actualTaskList).toEqual(expectedTaskList)
+  })
+})
