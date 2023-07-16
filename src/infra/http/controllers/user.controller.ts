87db0a9ff03a9b033fa01501c711ac86a5942354
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2023-07-16 08:49:22 -0300

    feat: add login route

--- src/infra/http/controllers/user.controller.ts
+++ src/infra/http/controllers/user.controller.ts
@@ -1,20 +1,24 @@
-import { Body, ConflictException, Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
 import { CreateUserUseCase } from '../../../domain/use-cases/user/create/create-user.use-case'
 import { CreateUserDto } from '../dtos'
-import { ConflictError } from 'src/domain/errors'
+import { LoginDto } from '../dtos/login.dto'
+import { LoginUseCase } from 'src/domain/use-cases/user/login/login.use-case'
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly loginUseCase: LoginUseCase,
+  ) {}
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
-    try {
-      return await this.createUserUseCase.execute(createUserDto)
-    } catch (error) {
-      if (error instanceof ConflictError) {
-        throw new ConflictException(error.messages)
-      } else throw error
-    }
+    return await this.createUserUseCase.execute(createUserDto)
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/login')
+  async login(@Body() loginDto: LoginDto) {
+    return await this.loginUseCase.execute(loginDto)
   }
 }
