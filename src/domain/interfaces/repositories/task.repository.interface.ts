0659be2ef60020be819912a986238a50a2ee3b91
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2023-07-16 18:48:40 -0300

    feat: ensure that ListTasksUseCase is able to filter by status if status on command is not null

--- src/domain/interfaces/repositories/task.repository.interface.ts
+++ src/domain/interfaces/repositories/task.repository.interface.ts
@@ -1,8 +1,10 @@
 import { Task } from 'src/domain/entities/task/task.entity'
+import { TaskStatus } from 'src/domain/enums'
 
 export interface TaskRepository {
   create(task: Task): Promise<void>
   list(userId: string): Promise<Task[]>
+  listByStatus(status: TaskStatus): Promise<Task[]>
   findById(id: string): Promise<Task>
   update(task: Task): Promise<void>
   delete(id: string): Promise<void>
--- src/domain/use-cases/task/list/list-tasks.use-case.ts
+++ src/domain/use-cases/task/list/list-tasks.use-case.ts
@@ -9,6 +9,9 @@ export class ListTasksUseCase {
     @Inject('TaskRepository') private readonly taskRepository: TaskRepository,
   ) {}
   async execute(command: ListTasksCommand): Promise<ListTasksOutput> {
+    if (command.status) {
+      return await this.taskRepository.listByStatus(command.status)
+    }
     return await this.taskRepository.list(command.userId)
   }
 }
